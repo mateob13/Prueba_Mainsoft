e94fd1932bcdaed965500a40d9027ec1e04c67c2
mateob13 <mateo.bedoya.b@gmail.com>
2022-08-04 19:35:53 -0500

    Se descomentarean los otros dos casos

--- Program.cs
+++ Program.cs
@@ -32,77 +32,77 @@ namespace test
 
         ////---------------------------------------------------------------------------------------------------------------------
 
-        //static int[] myArray = { 1, 2, 1, 3, 3, 1, 2, 1, 5, 1 };
+        static int[] myArray = { 1, 2, 1, 3, 3, 1, 2, 1, 5, 1 };
 
-        //public static void Main()
-        //{
-        //    var n = myArray.Length;
-        //    bool[] visited = new bool[n];
-        //    // Recorro de 1 a 5 para ver cu?ntas veces se repite cada valor
-        //    for (int i = 1; i <= 5; i++)
-        //    {
-        //        // Cantidad de veces que se repite i
-        //        var count = 0;
-        //        // se inicia en 0 porque hasta ahora no hay coincidencias
-        //        // Recorro el arreglo
-        //        for (int j = 0; j < n; j++)
-        //        {
-        //            // Skip this element if already processed
-        //            // Esto lo hago aqu? adentro porque es donde recorro el arreglo ahora
-        //            if (visited[j] == true)
-        //            {
-        //                continue;
-        //            }
-        //            // Si el valor en esa posici?n es el analizado (i) incremento
-        //            if (myArray[j] == i)
-        //            {
-        //                visited[j] = true;
-        //                count++;
-        //            }
-        //        }
-        //        // La i tiene el valor y count las veces que se repite
-        //        Console.Write(i.ToString() + ": ");
-        //        for (int k = 0; k < count; k++)
-        //        {
-        //            Console.Write('*');
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //}
+        public static void Main()
+        {
+            var n = myArray.Length;
+            bool[] visited = new bool[n];
+            // Recorro de 1 a 5 para ver cu?ntas veces se repite cada valor
+            for (int i = 1; i <= 5; i++)
+            {
+                // Cantidad de veces que se repite i
+                var count = 0;
+                // se inicia en 0 porque hasta ahora no hay coincidencias
+                // Recorro el arreglo
+                for (int j = 0; j < n; j++)
+                {
+                    // Skip this element if already processed
+                    // Esto lo hago aqu? adentro porque es donde recorro el arreglo ahora
+                    if (visited[j] == true)
+                    {
+                        continue;
+                    }
+                    // Si el valor en esa posici?n es el analizado (i) incremento
+                    if (myArray[j] == i)
+                    {
+                        visited[j] = true;
+                        count++;
+                    }
+                }
+                // La i tiene el valor y count las veces que se repite
+                Console.Write(i.ToString() + ": ");
+                for (int k = 0; k < count; k++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+        }
 
-        //////----------------------------------------------------------------------------------------------------------------------------------
+        ////----------------------------------------------------------------------------------------------------------------------------------
 
 
-        //static int[] myArray = { 1, 2, 2, 4, 5, 6, 7, 8, 8, 8 };
+        static int[] myArray = { 1, 2, 2, 4, 5, 6, 7, 8, 8, 8 };
 
-        //public static void Main()
-        //{
-        //    int maxNumero = myArray[0];
-        //    int maxVeces = 0;
+        public static void Main()
+        {
+            int maxNumero = myArray[0];
+            int maxVeces = 0;
 
-        //    int i = 0;
-        //    while (i < myArray.Length)
-        //    {
-        //        int numVeces = 0;
-        //        int j = 0;
-        //        while (j < myArray.Length)
-        //        {
-        //            if (myArray[j] == myArray[i]) numVeces++;
-        //            {
-        //                j++;
-        //            }
-        //        }
+            int i = 0;
+            while (i < myArray.Length)
+            {
+                int numVeces = 0;
+                int j = 0;
+                while (j < myArray.Length)
+                {
+                    if (myArray[j] == myArray[i]) numVeces++;
+                    {
+                        j++;
+                    }
+                }
 
-        //        if (numVeces > maxVeces)
-        //        {
-        //            maxNumero = myArray[i];
-        //            maxVeces = numVeces;
-        //        }
-        //        i++;
-        //    }
-        //    Console.WriteLine("Longest: " + maxVeces);
-        //    Console.WriteLine("Number: " + maxNumero);
-        //}
+                if (numVeces > maxVeces)
+                {
+                    maxNumero = myArray[i];
+                    maxVeces = numVeces;
+                }
+                i++;
+            }
+            Console.WriteLine("Longest: " + maxVeces);
+            Console.WriteLine("Number: " + maxNumero);
+        }
 
     }
     
